40f4cb5979cb7965af79958e1fc8af6437c4e915
rezun79 <rezun79@gmail.com>
2022-05-22 17:04:59 +0200

    test

--- main.tf
+++ main.tf
@@ -10,6 +10,11 @@ resource "azuread_service_principal" "example" {
   app_role_assignment_required = false
   owners                       = [data.azuread_client_config.current.object_id]
 }
-resource "azuread_service_principal_password" "example" {
-  service_principal_id = azuread_service_principal.example.object_id
+# resource "azuread_service_principal_password" "example" {
+#   service_principal_id = azuread_service_principal.example.object_id
+# }
+
+resource "azuread_application_password" "example" {
+  display_name = "terraformgenerated"
+  application_object_id = data.azuread_application.example.object_id
 }
